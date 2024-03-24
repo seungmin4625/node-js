fac063886e31a8f73b3aba68bebc1ce7ebfa8ca8
Seungmin Park <seungmin4625@gmail.com>
2024-03-24 23:50:20 +0900

    Change to EJS

--- app.js
+++ app.js
@@ -6,8 +6,16 @@ const expressHandlebars = require('express-handlebars');
 
 const app = express();
 
-app.engine('hbs', expressHandlebars());
-app.set('view engine', 'hbs');
+app.engine(
+  'hbs',
+  expressHandlebars({
+    layoutsDir: 'views/layouts',
+    defaultLayout: 'main-layout',
+    extname: 'hbs',
+  })
+);
+app.set('view engine', 'ejs');
+// app.set('view engine', 'hbs');
 // app.set('view engine', 'pug');
 app.set('views', 'views');
 
@@ -21,7 +29,7 @@ app.use('/admin', adminRoutes.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+  res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/404' });
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -10,7 +10,13 @@ const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeProduct: true,
+  });
 });
 
 // /admin/add-product => POST
